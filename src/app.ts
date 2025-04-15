d60abc846878a2516e617baf287ffa431a7d2816
Ericpv91 <ericpvallejo@gmail.com>
2025-04-15 19:08:00 +0200

    NocApp

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { envs } from "./config/plugins/envs.plugin"
+import { MongoDatabase } from "./data/mongo"
+import { PrismaClient } from "./generated/prisma"
+import { Server } from "./presentation/server"
+
+(async() => {
+    main()
+})()
+
+
+async function main(){
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    })
+
+
+
+    Server.start()
+}
